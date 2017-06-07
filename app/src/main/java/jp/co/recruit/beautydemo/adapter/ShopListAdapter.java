bf0dd808547b6f7bd53e7b85d90ca9c60e0cf406
田　甜 <tiantian@r.recruit.co.jp>
2017-06-07 16:28:25 +0900

    refine

--- app/src/main/java/jp/co/recruit/beautydemo/adapter/ShopListAdapter.java
+++ app/src/main/java/jp/co/recruit/beautydemo/adapter/ShopListAdapter.java
@@ -1,7 +1,6 @@
 package jp.co.recruit.beautydemo.adapter;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import jp.co.recruit.beautydemo.activity.R;
 import jp.co.recruit.beautydemo.model.ShopListEntity;
@@ -27,18 +23,6 @@ public class ShopListAdapter extends ArrayAdapter<ShopListEntity> {
     private List<ShopListEntity> list;
     private Context context;
     static private int ID = R.layout.cell_shop_list;
-
-    @BindView(R.id.shopTitleTextView)
-    TextView shopTitleTextView;
-
-    @BindView(R.id.imageView)
-    TextView imageView;
-
-    @BindView(R.id.addressTextView)
-    TextView addressTextView;
-
-    @BindView(R.id.accessTextView)
-    TextView accessTextView;
 
     public ShopListAdapter(@NonNull Context context, @NonNull List<ShopListEntity> objects) {
         super(context, ID, objects);
@@ -58,11 +42,11 @@ public class ShopListAdapter extends ArrayAdapter<ShopListEntity> {
         ButterKnife.bind(cell);
 
         ShopListEntity item = list.get(position);
-        shopTitleTextView = (TextView) cell.findViewById(R.id.shopTitleTextView);
+        TextView shopTitleTextView = (TextView) cell.findViewById(R.id.shopTitleTextView);
         shopTitleTextView.setText(item.name);
-        addressTextView = (TextView) cell.findViewById(R.id.addressTextView);
+        TextView addressTextView = (TextView) cell.findViewById(R.id.addressTextView);
         addressTextView.setText(item.address);
-        accessTextView = (TextView) cell.findViewById(R.id.accessTextView);
+        TextView accessTextView = (TextView) cell.findViewById(R.id.accessTextView);
         accessTextView.setText(item.access);
 
         return cell;
