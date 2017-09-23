b075ecc04681f9c4d2f6cf01f6187d35df541a20
Tian Tian <HollyTian@Tians-MacBook-Pro.local>
2017-09-23 22:28:11 +0900

    add resources

--- app/src/main/java/jp/co/recruit/beautydemo/activity/TopActivity.java
+++ app/src/main/java/jp/co/recruit/beautydemo/activity/TopActivity.java
@@ -1,10 +1,28 @@
 package jp.co.recruit.beautydemo.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by HollyTian on 2017/09/23.
  */
 
 public class TopActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_top);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.demo, menu);
+        return true;
+    }
 }
